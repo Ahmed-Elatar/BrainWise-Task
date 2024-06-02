938ae82b336ebcfe12c51fe040b680cf08605b7f
Ahmed-Elatar <a7med.elatar55@gmail.com>
2024-06-02 06:06:51 +0300

    edit stting file

--- BrainWise_Task/settings.py
+++ BrainWise_Task/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'brain_wise',
         'USER': 'postgres',
-        'PASSWORD': 'Ahmed2000-',
+        'PASSWORD': '*********',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
